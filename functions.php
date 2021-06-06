b73945ac6a27d65200d1f71d4dd90291ce453e87
Andreas Geyer <andreas@qbitone.de>
2021-06-06 13:13:09 +0200

    fixes enqueu scripts paths

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Quantum Child Theme functions and definitions.
  *
@@ -7,15 +8,16 @@
  * @package quantum-child
  */
 
-add_action( 'wp_enqueue_scripts', 'quantum_parent_theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'quantum_parent_theme_enqueue_styles');
 
 /**
  * Enqueue scripts and styles.
  */
-function quantum_parent_theme_enqueue_styles() {
-	wp_enqueue_style( 'quantum-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'quantum-child-style',
-		get_stylesheet_directory_uri() . '/style.css',
-		[ 'quantum-style' ]
+function quantum_parent_theme_enqueue_styles()
+{
+	wp_enqueue_style(
+		'quantum-child-style',
+		get_stylesheet_directory_uri() . '/assets/css/minified/style.min.css',
+		['quantum-style']
 	);
 }
