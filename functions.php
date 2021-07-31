c4d794aa070cf54784e4359aea457521fd2c2c88
Andreas Geyer <andreas@qbitone.de>
2021-07-31 14:46:05 +0200

    adds child theme constants

--- functions.php
+++ functions.php
@@ -8,6 +8,22 @@
  * @package quantum-child
  */
 
+// Exit if accessed directly.
+if (!defined('ABSPATH')) exit;
+
+
+define('QUANTUM_CHILD_VERSION', '0.1.0');
+define('QUANTUM_CHILD_SETTINGS', 'quantum-child-settings');
+define('QUANTUM_CHILD_DIR', trailingslashit(get_stylesheet_directory()));
+define('QUANTUM_CHILD_URI', trailingslashit(esc_url(get_stylesheet_directory_uri())));
+
+
+
+
+
+
+
+
 add_action('wp_enqueue_scripts', 'quantum_parent_theme_enqueue_styles');
 
 /**
