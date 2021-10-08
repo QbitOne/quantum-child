5c5d6aa5ca1acecd57a4fb4c92c23158d414ef21
Andreas Geyer <andreas@qbitone.de>
2021-10-08 14:05:07 +0200

    adds conditional to prevent possible fatal error
    - checks if class exists for using it

--- inc/core/enqueue-scripts.php
+++ inc/core/enqueue-scripts.php
@@ -10,11 +10,13 @@ if (!function_exists('qc_action_after_setup_theme')) :
     function qc_action_after_setup_theme(): void
     {
 
-        $qc_enqueue_sripts = new QT_Enqueue_Scripts(QUANTUM_CHILD_URI, 'qc', QUANTUM_CHILD_VERSION);
-        $qc_enqueue_sripts->add_style('style', 'assets/css/', ['qt-style']);
-        $qc_enqueue_sripts->add_login_style('login');
+        if (class_exists('QT_Enqueue_Scripts')) :
+            $qc_enqueue_sripts = new QT_Enqueue_Scripts(QUANTUM_CHILD_URI, 'qc', QUANTUM_CHILD_VERSION);
+            $qc_enqueue_sripts->add_style('style', 'assets/css/', ['qt-style']);
+            $qc_enqueue_sripts->add_login_style('login');
 
-        $qc_enqueue_sripts->enqueue();
+            $qc_enqueue_sripts->enqueue();
+        endif;
     }
     add_action('after_setup_theme', 'qc_action_after_setup_theme');
 endif;
